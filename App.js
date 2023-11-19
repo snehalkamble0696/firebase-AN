60b32ed84f0463238e36d6df325577c70dcf5d4f
snehalkamble0696 <77823641+snehalkamble0696@users.noreply.github.com>
2023-11-19 11:21:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import {Text, View} from 'react-native';
+import PushNoti from './src/Notification/PushNoti'
+const App = () => {
+  return (
+    <View>
+        <PushNoti/>
+    </View>
+  );
+};
+
+export default App;
